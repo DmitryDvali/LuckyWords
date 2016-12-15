1e38e9ad391cf6c57e9a34027892582471ecdbe8
DmitryDvali <Dimdimonish@yandex.ru>
2016-12-15 13:02:58 +0300

    Added 4 entites

---
+++ LuckyWords/LuckyWords/Entities/Square.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace LuckyWords.Entities
+{
+    class Square
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public TextBox TextBox { get; set; }
+    }
+}
---
+++ LuckyWords/LuckyWords/Entities/Word.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuckyWords.Entities
+{
+    class Word
+    {
+        public List<Square> Squares { get; set; }
+    }
+}
